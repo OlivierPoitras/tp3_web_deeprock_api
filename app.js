9edc06e74b13f4a6a4213db8dea82c322d5716ac
OlivierPoitras <poitrasolivier@gmail.com>
2022-05-12 18:59:11 -0400

    configDistante

--- app.js
+++ app.js
@@ -80,11 +80,11 @@ app.use(function (err, req, res, next) {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 mongoose
-  .connect('mongodb://127.0.0.1:27017/deepRockGalactic')
+.connect(process.env.DATABASE_URL)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log('Node.js est à l\'écoute sur le port %s ', PORT);
     });
   })
